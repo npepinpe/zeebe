640311bcfc93a958bb219957d222af2a7e87e142
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2021-03-22 12:58:37 +0700

    Revert "chore(broker): Use only the first address from the hostname command"
    This reverts commit 2627550d

--- docker/utils/startup.sh
+++ docker/utils/startup.sh
@@ -3,8 +3,7 @@
 # legacy support
 # This environment variable was used to set the gatewway cluster host in standalone and embedded mode.
 # Now, there are two dedicated environment variables for the two different deployment scenarios.
-# Print only first address because if we use an IPv6 there could be more than one addresses
-export ZEEBE_HOST=${ZEEBE_HOST:-$(hostname -i | awk '{print $1}')}
+export ZEEBE_HOST=${ZEEBE_HOST:-$(hostname -i)}
 # Legacy support
 
 if [ "$ZEEBE_STANDALONE_GATEWAY" = "true" ]; then
